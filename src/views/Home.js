5fe23572e3129a8908162489545b885984799992
Miguel Ángel <miguel_marba@hotmail.com>
2020-06-07 01:48:26 -0500

    add mensaje bienvenido

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../common/Layout';
+import authenticate from '../utils/authenticate';
 
 function Home() {
+    const {isAuthenticated, payload} = authenticate();
     return (
     <>
     <Layout title="Home" >
         <div className="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 className="h3 mb-0 text-gray-800">Bienvenido Miguel Ángel</h1>
-            <Link to="/eventos/create" className="d-none d-sm-inline-block btn btn-lg btn-success shadow-sm">
+            <h1 className="h3 mb-0 text-gray-800">Bienvenido { isAuthenticated ? payload.nombre: ''}</h1>
+            <Link to="/eventos/create" className="d-sm-inline-block btn btn-lg btn-success shadow-sm">
                     <i className="fas fa-plus fa-lg text-white-50"></i> Crear nueva ruta
             </Link>
         </div>
